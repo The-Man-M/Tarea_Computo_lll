54a9792dd7e83bf872a4de6292201a7864025e97
manuelmv15 <mv23036@ues.edu.sv>
2023-10-27 06:57:44 -0600

    Primera subida

---
+++ README.md
@@ -0,0 +1,1 @@
+# Tarea de programacion Computo 3
---
+++ Tarea Computo lll/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tarea_Computo_lll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
